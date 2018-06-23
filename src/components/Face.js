4590d727a733b41ad4531769ea654cddf9ee145a
Yong Yu En, Elvis <ELVISYONG.16@ichat.sp.edu.sg>
2018-06-24 01:31:06 +0800

    Add selection for group in Face.js

--- src/components/Face.js
+++ src/components/Face.js
@@ -1,18 +1,42 @@
 import React from "react";
-import { Button } from "semantic-ui-react";
+import { Button, Grid, Form } from "semantic-ui-react";
 
 export default class Face extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      person_group_list: [],
+      person_group_id: ""
+    };
+
     this.onClickHandler = this.onClickHandler.bind();
   }
 
-  async onClickHandler() {
-    alert("Taking picture...");
+  componentDidMount() {
+    fetch("http://127.0.0.1:5000/person_group_list")
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        let list = new Array();
+        data.forEach(obj => {
+          let text = obj.personGroupId;
+          list.push({ text: text, value: text });
+        });
 
+        this.setState({
+          person_group_list: list
+        });
+      });
+  }
+
+  async onClickHandler() {
     const response = await fetch("http://127.0.0.1:5000/face", {
-      method: "POST"
+      method: "POST",
+      body: JSON.stringify({
+        person_group_id: this.state.person_group_id
+      })
     });
 
     const content = await response.json();
@@ -23,9 +47,31 @@ export default class Face extends React.Component {
 
   render() {
     return (
-      <Button color="teal" onClick={this.onClickHandler}>
-        TAKE PHOTO
-      </Button>
+      <Grid centered columns={2}>
+        <Grid.Column>
+          <Form>
+            <Form.Field>
+              <label>Select person group</label>
+              <Dropdown
+                placeholder="Select person group"
+                search
+                selection
+                fluid
+                options={this.state.person_group_list}
+                value={this.state.person_group_id}
+                onChange={() => {
+                  this.setState({ person_group_id: data.value });
+                }}
+              />
+            </Form.Field>
+            <Form.Field>
+              <Button color="teal" onClick={this.onClickHandler}>
+                TAKE PHOTO
+              </Button>
+            </Form.Field>
+          </Form>
+        </Grid.Column>
+      </Grid>
     );
   }
 }
