393c703e11419cf3996f0a4a13bb8febaf54f88e
Yong Yu En, Elvis <ELVISYONG.16@ichat.sp.edu.sg>
2018-06-24 22:59:02 +0800

    Change body of edit text

--- src/components/EditText.js
+++ src/components/EditText.js
@@ -16,11 +16,13 @@ export default class EditText extends React.Component {
     await this.setState({
       text: event.target.value
     });
-    console.log(this.state.text)
+    console.log(this.state.text);
     const response = await fetch("http://127.0.0.1:5000/lcd", {
       method: "POST",
-      'Content-Type': 'application/json',
-      body: this.state.text
+      "Content-Type": "application/json",
+      body: {
+        text: this.state.text
+      }
     });
   }
 
